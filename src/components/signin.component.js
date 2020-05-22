b7824bbfc6edc2d5ef455155f9d5931506986d13
Poul <poulnichols@outlook.com>
2020-05-22 14:45:18 -0700

    Updating branch

--- src/components/signin.component.js
+++ src/components/signin.component.js
@@ -1,50 +1,34 @@
 import React, { Component } from 'react';
+import ReactDOM from "react-dom";
+import GoogleLogin from 'react-google-login';
 
-
+const clientId = '293901008493-duqun9t52bo7dtjbo4n1900nite8sube.apps.googleusercontent.com'
 
 export default class SignIn extends Component {
 
   componentDidMount() {
     console.log('Loading')
-
     window.gapi.load('auth2', () => {
         window.gapi.auth2.init({
-            client_id: '293901008493-duqun9t52bo7dtjbo4n1900nite8sube.apps.googleusercontent.com'
+            client_id: clientId
         })
     })
     console.log('Api inited')
+    }
 
-  }
-
-
-
-//Adding button
+   
     render() {
         return (
             <div>
                 <h3>SignIn Page</h3>
                 <div class="g-signin2" data-onsuccess="onSignIn"></div>
-          
                 
-            </div>
+              </div>
         )
 
+        
        
-        <script>
-            function onSignIn(googleUser) {
-        var profile = googleUser.getBasicProfile();
-        console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
-        console.log('Name: ' + profile.getName());
-        console.log('Image URL: ' + profile.getImageUrl());
-        console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
-            }
-        </script>
-        
-    
-        
     }
 
-    
-      
     
 }
--- src/components/signout.component.js
+++ src/components/signout.component.js
@@ -4,6 +4,20 @@ import React, { Component } from 'react';
 
 export default class SignOut extends Component {
 
-  
+    render() {
+        return (
+            <div>
+                <h3>SignOut Page</h3>
+                <script>
+                var auth2 = gapi.auth2.getAuthInstance();
+                auth2.signOut().then(function ()
+                </script>
+                
+            </div>
+        )
+
+       
+        
+    }
 
 }
