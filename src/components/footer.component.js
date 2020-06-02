d5a2a14ec6beb64127dadb47ffd81f9536f447b9
RayWu222 <48284351+RayWu222@users.noreply.github.com>
2020-06-02 16:14:00 -0700

    Added Social Media to footer component

--- src/components/footer.component.js
+++ src/components/footer.component.js
@@ -3,32 +3,42 @@ import {ReactComponent as Logo} from './img/dexOwl.svg';
 export default class Footer extends Component{
     render(){
         return(
-            <div class = "footer-container">
+            <footer class = "footer-container">
             <div class = "footer links left">
                <Logo class = "footer-logo"/>
                </div>
                <div class = "footer-links">
                 <strong>Links</strong> 
                             <ul>
-                                <li><a href ="/" Home>Home</a></li> 
+                                <li><a class = "home" href ="/" Home>Home</a></li> 
                                 <li><a href ="/create" Create>Create</a></li> 
                                 <li><a href ="/list" List>List</a></li>
                                 <li><a href ="/search" Search>Search</a></li>
-                                <div class = "copy-colorblack">
-                                  <p>© 2020 All Rights Reserved ~ Authored by AD410 Group C</p>
-                                </div> 
+                                
                             </ul>
                </div>
-               <div class = "subject-right">
+               <div class = "footer-subject">
                <strong>Subjects</strong> 
                             <ul>
                                 <li><a href ="/" Math>Math</a></li> 
                                 <li><a href ="/create" Science>Science</a></li> 
                                 <li><a href ="/list" English>English</a></li>
-                                <li><a href ="/search" Search>Search</a></li>
                             </ul>
                </div>
+               <div class = "footer-social-media">
+               <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"></link>
+               <strong>Social Media:</strong> 
+                            <ul>
+                            <a href="#" class="fa fa-facebook"></a>
+                            <a href="#" class="fa fa-twitter"></a>
+                            <a href="#" class="fa fa-reddit"></a>
+                            <a href="#" class="fa fa-google"></a>
+                            </ul>
                </div>
+               <div class = "copy-colorblack">
+                                  <p>© 2020 All Rights Reserved ~ Authored by AD410 Group C</p>
+                                </div> 
+               </footer>
         )
     }
 }
