477773b4906c5c90f68828aaf027ef9707bbc41d
SherrodWare <kungskii88@gmail.com>
2020-06-14 14:10:48 -0700

    trying to figure out "const"

--- src/components/search-bar.component.js
+++ src/components/search-bar.component.js
@@ -8,6 +8,7 @@ export default class SearchBar extends Component {
     super(props);
     this.state = { searchResult: [], searchTerm: " error", instanceID: 0 };
   }
+  
  search = ("Sucess Search ")
 //Search cannot be null stil trying to figure this part out. 
 // Const can only be used in TypeScript files.
