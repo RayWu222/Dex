3ededd39774c9173c082a3838c1216887fd2307c
Prefabmango112 <owensbrandan@gmail.com>
2020-06-16 16:52:28 -0700

    Resolving Merge issue (incomplete)

--- src/App.js
+++ src/App.js
@@ -1,11 +1,6 @@
 import React, { Component } from "react";
-<<<<<<< HEAD
-import { BrowserRouter as Router, Route} from "react-router-dom";
-import "./main.css";
-=======
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import axios from "axios"
->>>>>>> 148c5b3a062fe1db99f124f549a10e29d5ab3756
 
 
 import NavBar from "./components/navbar.component";
@@ -14,17 +9,12 @@ import LandingPage from "./components/landing-page.component";
 import CreateFlashcardSet from "./components/create-flashcardset.component";
 import EditFlashcardSet from "./components/edit-flashcardset.component";
 import FlashcardSetList from "./components/flashcardset-list.component";
-<<<<<<< HEAD
 import SignIn from "./components/signin-pack.component";
 import SignOut from "./components/signout.component";
-import Profile from "./components/profile.component"
-import DeleteFlashcardSet from "./components/deleteCardSet.component"
-=======
+import Profile from "./components/profile.component";
+import DeleteFlashcardSet from "./components/deleteCardSet.component";
 import SearchPage from "./components/search.component";
-import SignIn from "./components/signin.component";
-import SignOut from "./components/signout.component";
 import Footer from "./components/footer.component";
->>>>>>> 148c5b3a062fe1db99f124f549a10e29d5ab3756
 
 
 import Browse from  "./components/browseLink.component";
@@ -50,29 +40,17 @@ class App extends Component {
       
       <div className="app-wrapper">
       <Router>
-<<<<<<< HEAD
-        <NavBar> </NavBar>
-        <br/>
-                <Route path="/" exact component={LandingPage} />
-                <Route path="/list" exact component={FlashcardSetList} />
-                <Route path="/edit/:id" component={EditFlashcardSet} />
-                <Route path="/create" component={CreateFlashcardSet} />
-                <Route path="/signout" component={SignOut} />
-                <Route path="/signin" component={SignIn} />
-                <Route path= "/user" component={Profile}/>
-                <Route path= "/deleteSet/:id" component={DeleteFlashcardSet}/>
-=======
         <NavBar/>
-        <Route path="/" exact component={LandingPage} />
-        <Route path="/list" exact component={FlashcardSetList} />
-        <Route path="/flashcardSet/search/:search" exact component={SearchPage} />
-        <Route path="/edit/:id" component={EditFlashcardSet} />
-        <Route path="/create" component={CreateFlashcardSet} />
-        <Route path="/signup" component={SignOut} />
-        <Route path="/signin" component={SignIn} />
-        <Route path="/browse" component={Browse} />
-        <Route path="/subject/:subject" component={Subject} />
->>>>>>> 148c5b3a062fe1db99f124f549a10e29d5ab3756
+          <Route path="/" exact component={LandingPage} />
+          <Route path="/list" exact component={FlashcardSetList} />
+          <Route path="/edit/:id" component={EditFlashcardSet} />
+          <Route path="/create" component={CreateFlashcardSet} />
+          <Route path="/signout" component={SignOut} />
+          <Route path= "/user" component={Profile}/>
+          <Route path="/flashcardSet/search/:search" exact component={SearchPage} />
+          <Route path="/signin" component={SignIn} />
+          <Route path="/browse" component={Browse} />
+          <Route path="/subject/:subject" component={Subject} />
       </Router>
       <Footer/>
       </div>
