a51ba83f14082177172779c28059136949994e4c
Prefabmango112 <owensbrandan@gmail.com>
2020-06-16 14:20:18 -0700

    Deleted commented out code

--- src/components/navbar.component.js
+++ src/components/navbar.component.js
@@ -46,26 +46,7 @@ export default class Navbar extends Component {
                             <a className="nav-right" href="/signin">Login</a>
                     </ul>
                 </nav>
-            </div> 
-            
-
-            /* Dropdown Code 
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-
-            <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                    Dropdown
-                    </a>
-                    <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                    <a class="dropdown-item" href="#">Action</a>
-                    <a class="dropdown-item" href="#">Another action</a>
-                    <div class="dropdown-divider"></div>
-                    <a class="dropdown-item" href="#">Something else here</a>
-                    </div>
-                </li>
-            */
+            </div>
         )
     }
 }
